03cafca17e234cd3c2eefaa9c766e78c940b72ee
chrisemenova <chrisemenova88@gmail.com>
2023-04-09 21:59:22 +0300

    ui test

--- src/test/java/tests/CreditPageTest.java
+++ src/test/java/tests/CreditPageTest.java
@@ -1,4 +1,45 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import data.DataHelper;
+import data.SQLHelper;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import page.HomePage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CreditPageTest {
+    public static String url = System.getProperty("sut.url");
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    public void openPage() {
+        open(url);
+    }
+
+    @AfterEach
+    public void cleanBase() {
+        SQLHelper.cleanDatabase();
+    }
+
+    //--Happy path--
+
+    @Test
+    void shouldPurchaseCreditAllFieldValidApprovedCard(){
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getApprovedCard());
+        credit.notificationSuccessIsVisible();
+        assertEquals("APPROVED", SQLHelper.getCreditPaymentStatus());
+    }
 }
