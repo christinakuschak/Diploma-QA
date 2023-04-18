7bea0b93d43f6a37746f3b9311d975c96c04e2be
chrisemenova <chrisemenova88@gmail.com>
2023-04-18 12:37:08 +0300

    ui test fix and add

--- src/test/java/tests/CreditPageTest.java
+++ src/test/java/tests/CreditPageTest.java
@@ -17,13 +17,16 @@ public class CreditPageTest {
     static void setUpAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
     @AfterAll
     static void tearDownAll() {
         SelenideLogger.removeListener("allure");
     }
 
     @BeforeEach
+
     public void openPage() {
+        System.setProperty("chromeoptions.args", "--remote-allow-origins=*");
         open(url);
     }
 
@@ -32,14 +35,97 @@ public class CreditPageTest {
         SQLHelper.cleanDatabase();
     }
 
-    //--Happy path--
-
     @Test
-    void shouldPurchaseCreditAllFieldValidApprovedCard(){
+    void shouldBuyCreditAllFieldValidApprovedCard() {
         var startPage = new HomePage();
         var credit = startPage.goToCreditPage();
         credit.fillData(DataHelper.getApprovedCard());
         credit.notificationSuccessIsVisible();
         assertEquals("APPROVED", SQLHelper.getCreditPaymentStatus());
+    }
+
+    @Test
+    void shouldBuyCreditAllFieldValidDeclinedCard() {
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getDeclinedCard());
+        credit.notificationErrorIsVisible();
+        assertEquals("DECLINED", SQLHelper.getCreditPaymentStatus());
+    }
+
+    @Test
+    void shouldBuyCreditWithNonExistCard() {
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getNonExistCard());
+        credit.notificationErrorIsVisible();
+        assertEquals(null, SQLHelper.getCreditPaymentStatus());
+    }
+
+    @Test
+    void shouldBuyCreditWithInvalidCardNumber() {
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getInvalidCardNumber());
+        credit.waitForWrongFormatMessage();
+    }
+
+    @Test
+    void shouldBuyCreditWithEmptyMonth() {
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getEmptyMonth());
+        credit.waitForWrongFormatMessage();
+    }
+
+    @Test
+    void shouldBuyCreditWithMonthOver12() {
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getMonthOver12());
+        credit.waitForWrongCardExpirationMessage();
+    }
+
+    @Test
+    void shouldBuyCreditWithEmptyFieldYear(){
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getEmptyYear());
+        credit.waitForWrongFormatMessage();
+    }
+    @Test
+    void shouldBuyCreditWithLastYear(){
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getLastYear());
+        credit.waitForCardExpiredMessage();
+    }
+    @Test
+    void shouldBuyCreditWithFutureYear(){
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getNotComingYear());
+        credit.waitForWrongCardExpirationMessage();
+    }
+    void shouldBuyCreditHolderIsEmpty(){
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getEmptyHolderCard());
+        credit.waitForValidationFieldMessage();
+    }
+
+    @Test
+    void shouldBuyCreditHolderIsRus(){
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getInvalidHolderRusCard());
+        credit.waitForWrongFormatMessage();
+    }
+    @Test
+    void shouldBuyCreditWithCvcIsEmpty(){
+        var startPage = new HomePage();
+        var credit = startPage.goToCreditPage();
+        credit.fillData(DataHelper.getEmptyCVC());
+        credit.waitForValidationFieldMessage();
     }
 }
--- src/test/java/tests/PaymentPageTest.java
+++ src/test/java/tests/PaymentPageTest.java
@@ -16,13 +16,16 @@ public class PaymentPageTest {
     static void setUpAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
     @AfterAll
     static void tearDownAll() {
         SelenideLogger.removeListener("allure");
 
     }
+
     @BeforeEach
     public void openPage() {
+        System.setProperty("chromeoptions.args", "--remote-allow-origins=*");
         String url = System.getProperty("sut.url");
         open(url);
     }
@@ -32,7 +35,6 @@ public class PaymentPageTest {
         SQLHelper.cleanDatabase();
     }
 
-    //Happy
     @Test
     void shouldBuyAllFieldValidApprovedCard() {
         var startPage = new HomePage();
@@ -41,4 +43,126 @@ public class PaymentPageTest {
         payment.notificationSuccessIsVisible();
         assertEquals("APPROVED", SQLHelper.getDebitPaymentStatus());
     }
-}
+
+    @Test
+    void shouldBuyAllFieldValidDeclinedCard() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getDeclinedCard());
+        payment.notificationErrorIsVisible();
+        assertEquals("DECLINED", SQLHelper.getDebitPaymentStatus());
+    }
+
+    @Test
+    void shouldBuyWithNonExistDebitCard() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getNonExistCard());
+        payment.notificationErrorIsVisible();
+        assertEquals(null, SQLHelper.getDebitPaymentStatus());
+    }
+
+    @Test
+    void shouldBuyInvalidDebitCard() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getInvalidCardNumber());
+        payment.waitForWrongFormatMessage();
+    }
+
+    @Test
+    void shouldBuyWithEmptyCardNumber() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getEmptyFieldCardNumber());
+        payment.waitForWrongFormatMessage();
+    }
+
+    @Test
+    void shouldBuyAllZeroNumberDebitCard() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getNonExistCardAllZero());
+        payment.notificationErrorIsVisible();
+        assertEquals(null, SQLHelper.getDebitPaymentStatus());
+    }
+
+    @Test
+    void shouldBuyWithMonthOver12() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getMonthOver12());
+        payment.waitForWrongCardExpirationMessage();
+    }
+
+    @Test
+    void shouldBuyWithMonthIsEmpty() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getEmptyMonth());
+        payment.waitForWrongFormatMessage();
+    }
+
+    @Test
+    void shouldBuyWithCardPastMonth() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getInvalidPastMonth());
+        payment.waitForWrongCardExpirationMessage();
+    }
+
+    @Test
+    void shouldBuyWithYearIsEmpty() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getEmptyYear());
+        payment.waitForWrongFormatMessage();
+    }
+
+    @Test
+    void shouldBuyWithFieldInvalidYear() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getNotComingYear());
+        payment.waitForWrongCardExpirationMessage();
+    }
+
+    @Test
+    void shouldBuyWithEmptyHolder() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getEmptyHolderCard());
+        payment.waitForValidationFieldMessage();
+    }
+
+    @Test
+    void shouldBuyWithHolderRus() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getInvalidHolderRusCard());
+        payment.waitForWrongFormatMessage();
+    }
+
+    @Test
+    void shouldBuyWithHolderOnlyNumbers() {
+        var startPage = new HomePage();
+        var payment = startPage.goToPaymentPage();
+        payment.fillData(DataHelper.getInvalidHolderNumbersCard());
+        payment.waitForWrongFormatMessage();
+    }
+        @Test
+        void shouldBuyWithCVCZeros () {
+            var startPage = new HomePage();
+            var payment = startPage.goToPaymentPage();
+            payment.fillData(DataHelper.getZeroNumberCVC());
+            payment.waitForWrongFormatMessage();
+        }
+        @Test
+        void shouldBuyWithEmptyCvcField () {
+            var startPage = new HomePage();
+            var payment = startPage.goToPaymentPage();
+            payment.fillData(DataHelper.getEmptyCVC());
+            payment.waitForValidationFieldMessage();
+        }
+    }
+
